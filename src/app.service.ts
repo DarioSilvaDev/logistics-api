e190bd2ce0c2d3817e2997e3d29d6784849a0cdd
DarioSilvaDev <dariosilva.dev@gmail.com>
2026-04-01 12:59:56 -0300

    dotenv & status endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { envs } from './config/envs.config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getStatus(): {status: string, apiName: string} {
+    return {
+      status: "ONLINE",
+      apiName: envs.package_name 
+    };
   }
 }
